5d7353d3e0118110689b614f8eaeed63c0e777e3
UrmilaChaudhary <113498822+UrmilaChaudhary@users.noreply.github.com>
2022-10-08 11:01:32 +0530

    Add files via upload

---
+++ Lab(6-10-22)/Practical_1/Largest_Number.java
@@ -0,0 +1,39 @@
+/*	Program: Create a program to find largest number of two numbers
+* 	Name : Urmila Chaudhary
+*	date : 6/10/22
+ * 
+ */
+
+package Practical_1;
+
+public class Largest_Number {
+
+	public static void main(String[] args) {
+				
+						//importing Scanner
+			java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+						//Printing values of first number
+				System.out.print("Enter the First number : ");
+		
+					int x = sc.nextInt();
+					
+						//Printing values of second number
+				System.out.print("Enter the Second number : ");
+			
+					int y = sc.nextInt();
+		
+						//using if else loop
+					if(x>y)	{
+			
+						System.out.println(x+" is bigger then "+y);
+					}
+
+					else	{
+			
+						System.out.println(x+" is bigger then "+y);
+		}
+
+	}
+
+	}
---
+++ Lab(6-10-22)/Practical_4/Parents.java
@@ -0,0 +1,12 @@
+package Practical_4;
+
+public class Parents {
+	
+	void singing()	{
+		
+			//printing values of singing
+		System.out.println("I love to sing BTS Song");
+		
+	}
+
+}
