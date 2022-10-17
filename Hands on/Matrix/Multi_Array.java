7751443bff6a3383a634650c8bddfb6fdee40833
UrmilaChaudhary <113498822+UrmilaChaudhary@users.noreply.github.com>
2022-10-17 14:37:10 +0530

    Update Multi_Array.java

--- Hands on/Matrix/Multi_Array.java
+++ Hands on/Matrix/Multi_Array.java
@@ -4,6 +4,8 @@
  */
 
 package MultiD_Array;
+
+//importing Scanner
 import java.util.*;
 public class Multi_Array {
 
@@ -12,14 +14,17 @@ public class Multi_Array {
 		int i, j, x, y; 
 		Scanner sc = new Scanner(System.in);
 		
+		//Taking input from user for enter number of rows
 		System.out.print("Enter the no of rows :");
 		x = sc.nextInt();
 		
+		//Taking input from user for enter number of columns
 		System.out.print("Enter the no of column : ");
 		y = sc.nextInt();
 		
 		int array[][] = new int[x][y];
 		
+		//Taking Number of array from user
 		System.out.println("Enter the no of array :");
 		for(i=0;i<x;i++)
 		{
@@ -30,11 +35,14 @@ public class Multi_Array {
 		}
 		
 		System.out.println();
+		
+		//Printing element of array is
 		System.out.println("Element of array is ");
 		for(i=0;i<x;i++)
 		{
 			for(j=0;j<y;j++)
 			{
+				//Printing values of array of i j
 				System.out.print(array[i][j]+" ");
 			}
 			
