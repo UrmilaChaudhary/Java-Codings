9cbc49d97e142f64b8d171e738a204b754d788b8
UrmilaChaudhary <113498822+UrmilaChaudhary@users.noreply.github.com>
2022-10-21 17:00:22 +0530

    Add files via upload

---
+++ Hands on/Matrix/String Reverse/String_Rev_Array.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class String_Rev_Array 
+{
+
+	public static void main(String[] args)
+	{
+		// creating 0bject of scanner 
+		Scanner sc = new Scanner(System.in);
+		
+		//Taking input from user
+		System.out.print("Enter word : ");
+		String x = sc.nextLine();
+		char[] y= x.toCharArray();
+		
+		//System.out.println();
+		
+		//Printing values of String in reverse
+		System.out.print("Reverse String is : ");
+		
+		//Using for loop
+		for(int i=y.length-1;i>=0;i--)
+		{
+			System.out.print(y[i]+"");
+		}
+	}
+
+}
